ddf30f7a7c196f58075d85b5d2a4009428992cb1
SharonNaemi <naemi_17@live.de>
2025-02-16 22:01:06 +0100

    finalize classical node style

--- src/components/nodes/position.tsx
+++ src/components/nodes/position.tsx
@@ -12,6 +12,7 @@ export const PositionNode = memo((node: Node) => {
 
   const xRef = useRef(null);
   const yRef = useRef(null);
+
   const handleXChange = (e) => {
     const value = e.target.value;
     console.log(value);
@@ -30,11 +31,8 @@ export const PositionNode = memo((node: Node) => {
     setX(value);
   };
 
-
-  // Validate y input
   const handleYChange = (e) => {
     const value = e.target.value;
-
     // Check if the first character is a letter or underscore
     if (!/^[a-zA-Z_]/.test(value) && value !== "") {
       setYError(true);
@@ -47,32 +45,32 @@ export const PositionNode = memo((node: Node) => {
 
   return (
     <div className="grand-parent">
-      <div className="w-[320px] h-[150px] rounded-full bg-gray-800 overflow-hidden border border-solid border-gray-700 shadow-md">
+      <div className="w-[320px] h-[150px] rounded-full bg-white overflow-hidden border border-solid border-gray-700 shadow-md">
         <div className="w-full bg-blue-300 text-black text-center font-semibold py-1 truncate">
           {data.dataType}
         </div>
         <div className="px-2 py-3 flex justify-center">
-          <div className="flex items-center mb-2 space-x-2">
-            <label htmlFor="x" className="text-white text-sm mr-2">Value</label>
+          <div className="flex items-center mb-2">
+            <label htmlFor="x" className="text-black text-sm mr-2">Value</label>
             <input
               ref={xRef}
               id="x"
               type="number"
-              className={`p-1 text-black opacity-75 text-sm rounded-none w-20 rounded-full text-center ${error ? 'bg-red-500 border-red-500' : 'bg-white border-gray-500'}`}
+              className={`p-1 text-black opacity-75 text-sm rounded-none w-20 rounded-full text-center border ${error ? 'bg-red-500 border-red-500' : 'bg-white border-gray-500'}`}
               value={x}
               placeholder="0"
-              step={data.dataType === "int"? 1 : 0.1}
+              step={data.dataType === "int" ? 1 : 0.1}
               onChange={handleXChange}
             />
           </div>
         </div>
 
-        <div className="absolute bottom-[35%] right-5 transform translate-y-1/2 flex items-center space-x-3 z-10">
-          <label htmlFor="y" className="text-white text-sm mr-2">Output</label>
+        <div className="absolute bottom-[35%] right-5 transform translate-y-1/2 flex items-center z-10">
+          <label htmlFor="y" className="text-black text-sm mr-2">Output</label>
           <input
             ref={yRef}
             id="y"
-            className={`p-1 text-black opacity-75 text-sm w-10 text-center rounded-full ${yError ? 'bg-red-500 border-red-500' : 'bg-white border-gray-500'}`}
+            className={`p-1 text-black opacity-75 text-sm w-10 text-center rounded-full border ${yError ? 'bg-red-500 border-red-500' : 'bg-white border-gray-500'}`}
             value={y}
             placeholder="a"
             onChange={handleYChange}
